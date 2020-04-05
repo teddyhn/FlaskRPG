fa805ca24f73b78ebb024fa3f757de0c63d18476
teddyhn <tngo97@gmail.com>
2020-04-05 14:31:44 -0700

    Updates backend API URL

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,4 +1,4 @@
 export const SPRITE_SIZE = 16
 export const MAP_HEIGHT = SPRITE_SIZE * 15
 export const MAP_WIDTH = SPRITE_SIZE * 20
-export const BE_URL = 'https://team-big-bosses-be.herokuapp.com/'
+export const BE_URL = 'https://flask-rpg-api.herokuapp.com/'
